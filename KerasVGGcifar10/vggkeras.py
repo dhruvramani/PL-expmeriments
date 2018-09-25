c3932a0d9c495f47788428e452344ee6c3025582
dhruvramani <dhruvramani98@gmail.com>
2018-09-26 00:15:20 +0530

    plt rm

--- KerasVGGcifar10/vggkeras.py
+++ KerasVGGcifar10/vggkeras.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
