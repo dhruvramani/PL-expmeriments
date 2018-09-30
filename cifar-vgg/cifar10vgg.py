4eaf9d291f99d47b77c3e88d589395b1468bab64
dhruvramani <dhruvramani98@gmail.com>
2018-09-30 14:55:30 +0530

    GPU usage

--- cifar-vgg/cifar10vgg.py
+++ cifar-vgg/cifar10vgg.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function
 import keras
 from keras.datasets import cifar10
@@ -12,6 +11,12 @@ from keras.layers.core import Lambda
 from keras import backend as K
 from keras import regularizers
 from advanced_activations import ParametricLog
+
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.3
+set_session(tf.Session(config=config))
 
 class cifar10vgg:
     def __init__(self,train=True):
