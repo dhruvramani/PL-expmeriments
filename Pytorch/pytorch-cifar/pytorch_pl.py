7291a64de4869117bdd1ee8f7a3970817b901686
dhruvramani <dhruvramani98@gmail.com>
2018-10-31 02:04:30 +0530

    ugh

--- Pytorch/pytorch-cifar/pytorch_pl.py
+++ Pytorch/pytorch-cifar/pytorch_pl.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
 class ParametricLog(torch.nn.Module):
     def __init__(self, shape):
         super(ParametricLog, self).__init__()
--- Pytorch/pytorch_pl.py
+++ Pytorch/pytorch_pl.py
@@ -2,11 +2,15 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 class ParametricLog(torch.nn.Module):
     def __init__(self, shape):
         super(ParametricLog, self).__init__()
         self.c1 = Variable(torch.randn(shape)) # TODO : change this
         self.c2 = Variable(torch.randn(shape))
+        self.c1 = self.c1.to(device)
+        self.c2 = self.c2.to(device)
 
     def forward(self, x):
         pos = torch.log(F.relu(x + self.c1) + 1)
