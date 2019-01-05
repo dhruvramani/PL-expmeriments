b3f318cc6a087b89b2ab69de50b773cb209ea521
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-01-05 19:08:18 +0530

    pl - relu

--- Pytorch/pytorch-cifar/pytorch_pl.py
+++ Pytorch/pytorch-cifar/pytorch_pl.py
@@ -20,7 +20,8 @@ class ParametricLog(torch.nn.Module):
             self.c2 = self.c2.to(device)
             self.start = 1
 
-        pos_v, neg_v = F.relu(x), (-(x - torch.abs(x)) * 0.5)
-        pos = torch.log(pos_v + torch.abs(self.c1) + 1) #* (pos_v / x)
-        neg = - torch.log(neg_v + torch.abs(self.c2) + 1) #* (neg_v / x)
-        return pos + neg
+        #pos_v, neg_v = F.relu(x), (-(x - torch.abs(x)) * 0.5)
+        #pos = torch.log(pos_v + self.c1 + 1) #* (pos_v / x)
+        #neg = - torch.log(neg_v + self.c2 + 1) #* (neg_v / x)
+
+        return F.relu(x) #pos + neg
